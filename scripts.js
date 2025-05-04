40198a2f56bba399ceddcb2dd218e9a5e0429508
sam-k76 <kamarthi.sam@gmail.com>
2025-05-04 03:22:10 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Placeholder for potential JavaScript functionality
-document.querySelector('.submit-btn').addEventListener('click', function(event) {
-    event.preventDefault();
-    alert('Message sent! Thank you for contacting Onyx Concierge.');
-});
