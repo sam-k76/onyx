7c511a9f0120232626fc258a4889a4ae8c247fb0
sam-k76 <kamarthi.sam@gmail.com>
2025-05-04 03:09:03 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Placeholder for potential JavaScript functionality
+document.querySelector('.submit-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Message sent! Thank you for contacting Onyx Concierge.');
+});
